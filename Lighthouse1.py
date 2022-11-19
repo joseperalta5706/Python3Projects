41a8aa20863911106aceb04c7bc46932009450dd
Jose Peralta <117617169+joseperalta5706@users.noreply.github.com>
2022-11-19 10:53:17 -0800

    Update Lighthouse1.py

--- Lighthouse1.py
+++ Lighthouse1.py
@@ -1,8 +1,9 @@
 """ Program to determine how far a boat can see a lighthouse at sea in miles """
-
+import math 
 def view_distance():
     # 'h' is the height of a lighthouse in feet, as a whole number
     h = 200
-    distance = sqrt(.8 * h)
-    print("A " + h + " foot tall lighthouse can be seen from " + distance + " miles away")
-    view_distance()
+    distance = math.sqrt(.8 * h)
+    print("A " + str(h) + " foot tall lighthouse can be seen from " + str(distance) + " miles away")
+ 
+view_distance()
