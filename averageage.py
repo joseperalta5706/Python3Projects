8b9c9ad25fe869872eff748ac9d1755add34304b
Jose Peralta <117617169+joseperalta5706@users.noreply.github.com>
2022-11-20 20:19:56 -0800

    Add files via upload

---
+++ averageage.py
@@ -0,0 +1,14 @@
+import math
+# Division with whole numbers excersize.
+
+# Ages of 3 peeople.
+age1 = 19
+age2 = 21
+age3 = 30
+
+# Add the three people's ages, seperately, then dividing by the amount of variables used to find their average.
+averageAge = age1 + age2
+averageAge = averageAge + age3
+averageAge = averageAge / 3
+
+print("The average of three ages is " + "%.2f" % averageAge)
