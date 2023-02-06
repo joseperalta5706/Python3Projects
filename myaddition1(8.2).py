0802d3c18c08617cfe302c1e67c6eac670996c9f
Jose Peralta <117617169+joseperalta5706@users.noreply.github.com>
2023-02-06 09:41:17 -0800

    Create myaddition1(8.2).py

---
+++ myaddition1(8.2).py
@@ -0,0 +1,20 @@
+import math, random
+# function is set up
+def additionProblem(topNumber, bottomNumber):
+  print("\n\n\n     ", topNumber, "+", bottomNumber, "= ", end = "") 
+# user inputs answer to randomly generated problems
+  userAnswer = int(input())
+  theAnswer = topNumber + bottomNumber 
+# check the answer
+if theAnswer == userAnswer: 
+    print("      Correct!") 
+  else:
+    print("      Very good, but a better answer is", theAnswer)
+# additionProblem ends here
+
+# 'topNumber' and 'bottomNumber' set to random.randint to generate random integers between 1-10 inclusively
+additionProblem(random.randint(1,10), random.randint(1,10))
+additionProblem(random.randint(1,10), random.randint(1,10))
+additionProblem(random.randint(1,10), random.randint(1,10))
+additionProblem(random.randint(1,10), random.randint(1,10))
+additionProblem(random.randint(1,10), random.randint(1,10))
