1b8c117f2c0199dd8a9b57b72795fc86c0ce98e1
Jose Peralta <117617169+joseperalta5706@users.noreply.github.com>
2023-02-06 14:17:57 -0800

    Update myaddition1(8.2).py

--- myaddition1(8.2).py
+++ myaddition1(8.2).py
@@ -7,9 +7,9 @@ def additionProblem(topNumber, bottomNumber):
   theAnswer = topNumber + bottomNumber 
 # check the answer
 if theAnswer == userAnswer: 
-    print("      Correct!") 
-  else:
-    print("      Very good, but a better answer is", theAnswer)
+  print("      Correct!") 
+else:
+  print("      Very good, but a better answer is", theAnswer)
 # additionProblem ends here
 
 # 'topNumber' and 'bottomNumber' set to random.randint to generate random integers between 1-10 inclusively
