7204332641d7151634ab7c5f4157d600b6959400
Jose Peralta <117617169+joseperalta5706@users.noreply.github.com>
2023-04-26 18:01:09 -0700

    Create mycity2(11.1).py

---
+++ mycity2(11.1).py
@@ -0,0 +1,8 @@
+days = [60, 61, 62, 63, 64]
+print("Fort Bragg, California forecast temperatures:")
+print("Monday, Aug 25, " + str(days[0]) + " degrees")
+print("Tuesday, Aug 26 " + str(days[1]) + " degrees")
+print("Wednesday, Aug 27, " + str(days[2]) + " degrees")
+print("Thursday, Aug 28, " + str(days[3]) + " degrees")
+print("Friday, Aug 29, " + str(days[4]) + " degrees")
+print("Source: me")
